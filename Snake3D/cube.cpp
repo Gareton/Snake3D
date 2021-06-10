c3eebe78c5ac208d276a32ff89a5f0fac996d8e0
Gareton <37975712+Gareton@users.noreply.github.com>
2021-06-10 14:43:10 +0300

    Changed types in cube.h and cube.cpp to skTypes

--- Snake3D/cube.cpp
+++ Snake3D/cube.cpp
@@ -1,6 +1,6 @@
 #include "cube.h"
 
-Cube::Cube(float_t size, const gmt::vec3& position, const gmt::vec4 color)
+Cube::Cube(sk_float size, const gmt::vec3& position, const gmt::vec4 color)
 	: _cube({ size, size, size }, position, color)
 {}
 
@@ -9,7 +9,7 @@ void Cube::draw(const gmt::mat4& projection, const gmt::mat4& view)
 	_cube.draw(projection, view);
 }
 
-void Cube::update(double_t deltaTime) {}
+void Cube::update(sk_double deltaTime) {}
 
 void Cube::setPosition(const gmt::vec3& position)
 {
--- Snake3D/cube.h
+++ Snake3D/cube.h
@@ -3,14 +3,15 @@
 #include "gmath.h"
 #include "drawable.h"
 #include "straightParallelepiped.h"
+#include "skTypes.h"
 
 class Cube : Drawable
 {
 public:
-	explicit Cube(float_t size, const gmt::vec3& position = { 0.0f, 0.0f, 0.0f }, 
+	explicit Cube(sk_float size, const gmt::vec3& position = { 0.0f, 0.0f, 0.0f }, 
 				  const gmt::vec4 color = {0.0f, 0.0f, 0.0f, 1.0f});
 	void draw(const gmt::mat4& projection, const gmt::mat4& view);
-	void update(double_t deltaTime);
+	void update(sk_double deltaTime);
 
 	void setPosition(const gmt::vec3& position);
 private:
