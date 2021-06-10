eecb6f1bb88d0e805d347e904cd365417da6ac69
Gareton <37975712+Gareton@users.noreply.github.com>
2021-06-10 14:26:03 +0300

    Added skTypes.h

---
+++ Snake3D/skTypes.h
@@ -0,0 +1,12 @@
+#ifndef SK_TYPES_H
+#define SK_TYPES_H
+#include <glad/glad.h>
+
+using sk_int = GLint;
+using sk_uint = GLuint;
+using sk_float = GLfloat;
+using sk_double = GLdouble;
+using sk_char = GLchar;
+using sk_uchar = GLubyte;
+
+#endif
