b9f43248e61ab26fa03be0c3b793de94615785e9
Gareton <37975712+Gareton@users.noreply.github.com>
2021-06-11 06:21:01 +0300

    Optimized VoxelSphere rendering from O(N^3) to O(6 * N^2). Now it renders only sphere voxels, but not whole ball.

--- Snake3D/voxelSphere.cpp
+++ Snake3D/voxelSphere.cpp
@@ -5,20 +5,32 @@ VoxelSphere::VoxelSphere(const gmt::vec3& pos, sk_uint radius, sk_float voxelSiz
 	_cube(voxelSize, gmt::vec3(0.0f), gmt::vec4{ color.x, color.y, color.z, 1.0f })
 {}
 
-void VoxelSphere::draw(const gmt::mat4& projection, const gmt::mat4& view) 
+void VoxelSphere::draw(const gmt::mat4& projection, const gmt::mat4& view)
 {
 	sk_int intRadius = _radius;
 
-	for(sk_int i = -intRadius; i <= intRadius; ++i)
-		for(sk_int j = -intRadius; j <= intRadius; ++j)
-			for(sk_int k = -intRadius; k <= intRadius; ++k)
-				if (i * i + j * j + k * k <= gmt::sqr(_radius))
-				{
-					gmt::vec3 voxelPos = gmt::vec3{ 1.0f * i, 1.0f * j, 1.0f * k } * _voxelSize;
+	for (sk_int i = -intRadius; i <= intRadius; ++i)
+		for (sk_int j = -intRadius; j <= intRadius; ++j)
+			if (i * i + j * j <= gmt::sqr(intRadius))
+				for(sk_int kSign = -1; kSign <= 1; kSign += 2)
+					{
+						sk_int k = std::floor(std::sqrt(gmt::sqr(intRadius) - i * i - j * j)) * kSign;
 
-					_cube.setPosition(voxelPos);
-					_cube.draw(projection, view);
-				}
+						gmt::vec3 voxelPos = gmt::vec3{ 1.0f * i, 1.0f * j, 1.0f * k } * _voxelSize;
+
+						_cube.setPosition(voxelPos);
+						_cube.draw(projection, view);
+
+						voxelPos = gmt::vec3{ 1.0f * k, 1.0f * j, 1.0f * i } *_voxelSize;
+
+						_cube.setPosition(voxelPos);
+						_cube.draw(projection, view);
+
+						voxelPos = gmt::vec3{ 1.0f * i, 1.0f * k, 1.0f * j } *_voxelSize;
+
+						_cube.setPosition(voxelPos);
+						_cube.draw(projection, view);
+					}
 }
 
 void VoxelSphere::update(sk_double deltaTime) {}
